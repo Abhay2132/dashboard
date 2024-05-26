c886ade7fd8cbfc3311abc34c89bdceabc6329ad
abhay2132 <abhaybro768@gmail.com>
2024-05-26 18:45:01 +0530

    hehe

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.BASEPATH ? process.env.BASEPATH : "",
-    images:{
-        loader:"custom",
-        loaderFile:"/util/image-loader.mjs",
-    }
+    // basePath: process.env.BASEPATH ? process.env.BASEPATH : "",
+    // images:{
+    //     loader:"custom",
+    //     loaderFile:"/util/image-loader.mjs",
+    // }
+    basePath : '/dashboard'
 };
 
 export default nextConfig;
